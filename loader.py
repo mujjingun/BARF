0c10ca3c49732e39aa180313b7dfb41a00687b14
Daniel Geon Park <mujjingun@gmail.com>
2022-04-18 19:57:52 +0900

    Add loader boilerplate

--- loader.py
+++ loader.py
@@ -1,1 +1,10 @@
- 
+
+def load_dataset(dataset_type):
+    """
+    Loads dataset.
+    :param dataset_type: "llff" or "blender"
+    :return: a tuple (images, poses, render_poses, hwf, i_split)
+    """
+    # TODO
+
+    return images, poses, render_poses, hwf, i_split
