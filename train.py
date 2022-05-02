042414a91de27f9edaeed0f003619cf87ebadf7a
KorMachine <rlaguswls129@naver.com>
2022-05-02 03:10:30 +0000

    hyunjin_update

--- train.py
+++ train.py
@@ -18,11 +18,12 @@ def train_nerf(model,
         optimizer.zero_grad()
 
         # TODO: sample points on ray
-        world_o, world_d = get_rays(hwf,c2w) # world_o : (3), world_d (H x W x 3)
-
         i_train = i_split[0]
         train_idx = np.random.choice(i_train, 1)
         train_im = images[train_idx] # H x W x 3
+        c2w = poses[train_idx]
+
+        world_o, world_d = get_rays(hwf,c2w) # world_o : (3), world_d (H x W x 3)
 
         world_d_flatten = world_d.reshape(-1,3)
         gt_flatten = train_im.reshape(-1,3)
