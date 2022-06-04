1cf4a4f2e12d2296ebcef2b68f503bde4e069446
KorMachine <rlaguswls129@naver.com>
2022-06-04 15:23:56 +0000

    add ssim lpips

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ import imageio as io
 import math
 import pytorch3d.transforms
 from utils import *
+from skimage.metrics import structural_similarity as calc_ssim
+from lpips import LPIPS
 
 mse_loss = lambda output, gt: torch.mean((output - gt) ** 2)
 calc_psnr = lambda mse: -10 * torch.log10(mse)
@@ -53,6 +55,10 @@ def test_nerf(model, pos_encoder, dir_encoder,
     test_poses_inv = to_matrix(R_aligned, t_aligned)
     test_poses = expand(invert(test_poses_inv))
 
+    psnr_ = []
+    ssim_ = []
+    lpips_ = []
+    calc_lpips = LPIPS(net='alex').to(device)
     for idx in i_test:
         test_im = images[idx]
         gt_flatten = torch.from_numpy(test_im.reshape(-1, 3)).to(device)
@@ -85,12 +91,24 @@ def test_nerf(model, pos_encoder, dir_encoder,
 
         mse = mse_loss(color, gt_flatten)
         psnr = calc_psnr(mse)
+        psnr_.append(psnr.cpu().numpy())
 
         print(f"[Image index {idx}] PSNR : {psnr.item()}")
 
         gt_im = gt_flatten.reshape(hwf[0], hwf[1], 3)
 
         color = color.reshape(hwf[0], hwf[1], 3)
+
+        ssim = calc_ssim(color.clone().cpu().numpy(), gt_im.clone().cpu().numpy(), channel_axis = 2, multichannel=True)
+        ssim_.append(ssim)
+
+        gt_im = gt_im.type('torch.cuda.FloatTensor')
+        lpips = calc_lpips(color.transpose(1,2).transpose(0,1).unsqueeze(0), gt_im.transpose(1,2).transpose(0,1).unsqueeze(0))
+        lpips_.append(lpips.cpu().numpy())
+
+        print(f"[Image index {idx}] PSNR : {psnr.item()} | SSIM : {ssim} | LPIPS : {lpips.item()}")
+        
+        
         depth = depth.reshape(hwf[0], hwf[1])
 
         concat = torch.cat([gt_im, color], 1)
@@ -101,3 +119,9 @@ def test_nerf(model, pos_encoder, dir_encoder,
         io.imsave(f"{args.basedir}/test_gt_img/{idx:03d}.png", concat.cpu().numpy())
         io.imsave(f"{args.basedir}/test_img/{idx:03d}.png", color.cpu().numpy())
         io.imsave(f"{args.basedir}/test_depth/{idx:03d}.png", depth.cpu().numpy())
+    psnr_ = np.mean(np.stack(psnr_,0))
+    ssim_ = np.mean(np.stack(ssim_,0))
+    lpips_ = np.mean(np.stack(lpips_,0))
+
+    metrics = np.array([psnr_,ssim_,lpips_])
+    np.save(f"{args.basedir}/metric.npy",metrics)
